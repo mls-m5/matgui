0f412aab502141f30e24d92e7e178d6dfa41d4ff
Mattias Lasersköld <mattias.l.sk@gmail.com>
2018-09-09 12:16:42 +0200

    Removed initialization in constructor in texture-class

--- src/texture.cpp
+++ src/texture.cpp
@@ -58,30 +58,11 @@ unsigned int createTextureFromFile(const std::string filename) {
 
 
 Texture::Texture() {
-	glGenTextures(1, &_textureId);
-	glBindTexture(GL_TEXTURE_2D, _textureId);
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+//	glGenTextures(1, &_textureId);
+//	glBindTexture(GL_TEXTURE_2D, _textureId);
+//	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+//
 
-	//unsigned char pixels[_width * _height * 3];
-	//for (int x = 0; x < _width; ++x) {
-	//	for (int y = 0; y < _height; ++y) {
-	//		for (int c = 0; c < 3; ++c) {
-	//			pixels[(x + y * _width) * 3 + c] = 100 * x / _width + ((y % 10) > 5? 0 : 100);
-	//		}
-	//	}
-	//}
-
-	//glTexImage2D(
-	//		GL_TEXTURE_2D,         // target
-	//		0,                     // level, 0 = base, no minimap,
-	//		GL_RGB,                // internalformat
-	//		_width,     // width
-	//		_height,    // height
-	//		0,                     // border, always 0 in OpenGL ES
-	//		GL_RGB,                // format
-	//		GL_UNSIGNED_BYTE,      // type
-	//		pixels
-	//);
 
 }
 
