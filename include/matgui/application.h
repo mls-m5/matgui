02c6d14420ea648d8d04392b14d37d210c8559f4
Mattias Lasersköld <mattias.l.sk@gmail.com>
2020-03-09 14:13:30 +0100

    Refactor

--- include/matgui/application.h
+++ include/matgui/application.h
@@ -5,7 +5,6 @@
  *      Author: Mattias Larsson Sköld
  */
 
-
 #pragma once
 
 #include "matsig.h"
@@ -14,58 +13,57 @@ namespace MatGui {
 
 class Application {
 public:
-	// Constructor sets up environment
-	Application(int argc, char **argv);
+    // Constructor sets up environment
+    // Arguments are used to to use program arguments as --scale
+    Application(int argc = 0, char **argv = nullptr);
 
-	// Destructor tears down environment
-	// Exception is for emscripten when the destructor does nothing
-	virtual ~Application();
-	static void mainLoop();
-	static bool handleEvents();
+    // Destructor tears down environment
+    // Exception is for emscripten when the destructor does nothing
+    virtual ~Application();
+    static void mainLoop();
+    static bool handleEvents();
 
-	static class Window *getWindow(unsigned int w);
+    static class Window *getWindow(unsigned int w);
 
-	// Stops the main loop
-	static void quit();
+    // Stops the main loop
+    static void quit();
 
-	// note: frameUpdate signal is moved to window
+    // note: frameUpdate signal is moved to window
 
-	// Scale is a global value that sets the scale for graphics.
-	// This is to enable same window size on hd screens
-	// Change by sending for example "--scale 2" as arguments on construction
-	static float Scale();
+    // Scale is a global value that sets the scale for graphics.
+    // This is to enable same window size on hd screens
+    // Change by sending for example "--scale 2" as arguments on construction
+    static float Scale();
 
-	// Used to set scale
-	// If used after crating any window the result is undefined
-	static void Scale(float scale);
+    // Used to set scale
+    // If used after crating any window the result is undefined
+    static void Scale(float scale);
 
-	// Continuous updates is when the screen updates as fast as possible
-	// (in sync with the screen updates
-	// When disable window::invalidate triggers a redraw
-	// Also any input event triggers redraw of the window by default
-	static void ContinuousUpdates(bool state);
+    // Continuous updates is when the screen updates as fast as possible
+    // (in sync with the screen updates
+    // When disable window::invalidate triggers a redraw
+    // Also any input event triggers redraw of the window by default
+    static void ContinuousUpdates(bool state);
 
-	// Sets if windows should be invalidated on input events
-	// if false window::invalidate needs to be called to redraw
-	static void InvalidateOnEvent(bool state);
+    // Sets if windows should be invalidated on input events
+    // if false window::invalidate needs to be called to redraw
+    static void InvalidateOnEvent(bool state);
 
-	// Functions to convert between key and description string
-	static std::string GetKeyNameFromScancode(int scancode);
-	static std::string GetKeyNameFromKey(int scancode);
-	static int GetKeyFromName(const std::string &name);
-	static int GetScancodeFromName(const std::string &name);
-	static int GetScancodeFromKey(int key);
-	static int GetKeyFromScancode(int scancode);
+    // Functions to convert between key and description string
+    static std::string GetKeyNameFromScancode(int scancode);
+    static std::string GetKeyNameFromKey(int scancode);
+    static int GetKeyFromName(const std::string &name);
+    static int GetScancodeFromName(const std::string &name);
+    static int GetScancodeFromKey(int key);
+    static int GetKeyFromScancode(int scancode);
 
-protected:
+private:
+    // Functions used internally to handle windows
+    static void addWindow(class Window *);
+    static void removeWindow(class Window *);
+    static inline void innerLoop();
 
-	//Functions used internally to handle windows
-	static void addWindow(class Window*);
-	static void removeWindow(class Window*);
-	static inline void innerLoop();
-
-	friend class Window;
+    friend class Window;
 };
 
-}  // namespace MatGui
-
+} // namespace MatGui
--- src/application.cpp
+++ src/application.cpp
@@ -7,227 +7,239 @@
 
 #ifndef __ANDROID__
 
+#include "matgui/application.h"
+#include "matgui/draw.h"
+#include "matgui/window.h"
+#include "signal.h"
+#include "windowdata.h"
+
 #include <SDL2/SDL.h>
 #include <iostream>
-#include "matgui/application.h"
-#include "matgui/commonsdl.h"
-#include "signal.h"
-#include "matgui/window.h"
-#include "matgui/draw.h"
-#include "windowdata.h"
 
 #ifdef __EMSCRIPTEN__
 #include <emscripten.h>
 #endif
 
 using namespace std;
+using namespace MatGui;
+
+namespace {
+
+// Some global variables
+std::list<Window *> windows;
+Window *activeWindow = nullptr;
+Application *application = nullptr;
+float scale = 1.f;
+bool continuousUpdates = true;
+bool invalidateOnEvent = true;
+
+bool running = false;
+uint32_t lastTick = 0; // used to keep track of framerate
+
+} // namespace
 
 namespace MatGui {
 
-static std::list<Window *> windows;
-static Window* activeWindow = nullptr;
-static Application *application = nullptr;
-static float scale = 1.f;
-static bool continuousUpdates = true;
-static bool invalidateOnEvent = true;
+Application::Application(int argc, char **argv) {
+    application = this;
+    MatSig::setMainThread();
 
-static bool running = false;
-static uint32_t lastTick = 0; // used to keep track of framerate
+    if (running == true) {
+        throw std::runtime_error(
+            "Application already created in other part of program");
+    }
 
-Application::Application(int argc, char** argv) {
-	application = this;
-	MatSig::setMainThread();
+    for (int i = 0; i + 1 < argc; ++i) {
+        if (argv[i] == string("--scale")) {
+            ++i;
+            scale = atof(argv[i]);
+        }
+    }
 
-	if (running == true) {
-		throw std::runtime_error("Application already created in other part of program");
-	}
-
-	for (int i = 0; i + 1 < argc; ++i) {
-		if (argv[i] == string("--scale")) {
-			++i;
-			scale = atof(argv[i]);
-		}
-	}
-
-    if (SDL_Init(SDL_INIT_VIDEO) < 0) /* Initialize SDL's Video subsystem */
-        sdldie("Unable to initialize SDL"); /* Or die on error */
+    if (SDL_Init(SDL_INIT_VIDEO) < 0) {
+        SDL_Quit();
+        throw std::runtime_error("could not initialize sdl video");
+    }
 }
 
 void Application::quit() {
-	running = false;
+    running = false;
 }
 
-//// This separation is to make it possible to run on single threaded platforms
-//// such as javascript with emscripten
-//void innerLoopPublic(void *application) {
-//	((Application*)application)->innerLoop();
-//}
-
 inline void Application::innerLoop() {
-	auto newTime = SDL_GetTicks();
-	auto passedTime = (double) (newTime - lastTick) / 1000.;
-	lastTick = newTime;
+    auto newTime = SDL_GetTicks();
+    auto passedTime = (double)(newTime - lastTick) / 1000.;
+    lastTick = newTime;
 
-	auto shouldRedraw = continuousUpdates;
+    auto shouldRedraw = continuousUpdates;
 
-	if (!shouldRedraw) {
-		for (auto it: windows){
-			if (it->invalid()) {
-				shouldRedraw = true;
-			}
-		}
-	}
-
-//	if (shouldRedraw && frameUpdate) {
-//		//Handle gives the developer the possibility to do things each frame
-//		frameUpdate.directCall(passedTime);
-//	}
+    if (!shouldRedraw) {
+        for (auto it : windows) {
+            if (it->invalid()) {
+                shouldRedraw = true;
+            }
+        }
+    }
 
 #ifndef __EMSCRIPTEN__
-	if (!shouldRedraw && !continuousUpdates) {
-		SDL_Delay(10); //Limit cpu usage on idle
-	}
+    if (!shouldRedraw && !continuousUpdates) {
+        SDL_Delay(10); // Limit cpu usage on idle
+    }
 #endif
 
-	for (auto it: windows) {
-		if (continuousUpdates || it->invalid()) {
-			it->clear();
-			it->frameUpdate.directCall(passedTime);
-			it->draw();
-			it->swap();
-			it->invalid(false);
-		}
-	}
+    for (auto it : windows) {
+        if (continuousUpdates || it->invalid()) {
+            it->clear();
+            it->frameUpdate.directCall(passedTime);
+            it->draw();
+            it->swap();
+            it->invalid(false);
+        }
+    }
 
-	if (handleEvents()) {
-		running = false;
-		return;
+    if (handleEvents()) {
+        running = false;
+        return;
 #ifdef __EMSCRIPTEN__
-		emscripten_cancel_main_loop();
+        emscripten_cancel_main_loop();
 #endif
-	}
+    }
 
-	MatSig::flushSignals();
+    MatSig::flushSignals();
 }
 
 void Application::mainLoop() {
-	running = true;
-	lastTick = SDL_GetTicks();
+    running = true;
+    lastTick = SDL_GetTicks();
 #ifdef __EMSCRIPTEN__
     emscripten_set_main_loop(innerLoop, 60, 1);
 #else
-	while (running) {
-		innerLoop();
-	}
+    while (running) {
+        innerLoop();
+    }
 #endif
 }
 
 Window *Application::getWindow(unsigned int w) {
-	for (auto it: windows) {
-		if (w == it->_windowData->windowId) {
-			return it;
-		}
-	}
-	return nullptr;
+    for (auto it : windows) {
+        if (w == it->_windowData->windowId) {
+            return it;
+        }
+    }
+    return nullptr;
 }
 
 bool Application::handleEvents() {
-	SDL_Event event;
-	while (SDL_PollEvent(&event)) {
-		if (event.type == SDL_QUIT) {
-			return true;
-		}
+    SDL_Event event;
+    while (SDL_PollEvent(&event)) {
+        if (event.type == SDL_QUIT) {
+            return true;
+        }
 
-		auto window = getWindow(event.window.windowID);
-		if (window) {
-			auto redrawEvent = invalidateOnEvent; //If the event should trigger a redraw
-			if (event.type == SDL_WINDOWEVENT) {
+        auto window = getWindow(event.window.windowID);
+        if (window) {
+            auto redrawEvent =
+                invalidateOnEvent; // If the event should trigger a redraw
+            if (event.type == SDL_WINDOWEVENT) {
 
-				switch (event.window.event) {
-				case SDL_WINDOWEVENT_CLOSE:
-					if (not window->onRequestClose()) {
-						window->hide();
-					}
-					break;
-				case SDL_WINDOWEVENT_LEAVE:
-					window->onPointerLeave();
-					break;
-				case SDL_WINDOWEVENT_RESIZED:
-					window->onResize(event.window.data1, event.window.data2);
-					break;
-				case SDL_WINDOWEVENT_FOCUS_GAINED:
-					activeWindow = window;
-					break;
-				case SDL_WINDOWEVENT_FOCUS_LOST:
-					if (activeWindow == window) {
-						activeWindow = nullptr;
-					}
-					break;
-				default:
-					redrawEvent = false;
-				}
-			}
-			else {
+                switch (event.window.event) {
+                case SDL_WINDOWEVENT_CLOSE:
+                    if (not window->onRequestClose()) {
+                        window->hide();
+                    }
+                    break;
+                case SDL_WINDOWEVENT_LEAVE:
+                    window->onPointerLeave();
+                    break;
+                case SDL_WINDOWEVENT_RESIZED:
+                    window->onResize(event.window.data1, event.window.data2);
+                    break;
+                case SDL_WINDOWEVENT_FOCUS_GAINED:
+                    activeWindow = window;
+                    break;
+                case SDL_WINDOWEVENT_FOCUS_LOST:
+                    if (activeWindow == window) {
+                        activeWindow = nullptr;
+                    }
+                    break;
+                default:
+                    redrawEvent = false;
+                }
+            }
+            else {
 
-				switch (event.type) {
-				case SDL_MOUSEMOTION:
-				{
-					auto &e = event.motion;
-					window->onPointerMove(0, (double)e.x / scale, (double)e.y / scale, e.state);
-				}
-				break;
-				case SDL_MOUSEBUTTONDOWN:
-				{
-					auto &e = event.button;
-					auto button = MouseButton(1 << (e.button - 1)); //Make one bit for each button
-					window->onPointerDown(0, button, (double)e.x / scale, (double)e.y / scale);
-				}
-				break;
+                switch (event.type) {
+                case SDL_MOUSEMOTION: {
+                    auto &e = event.motion;
+                    window->onPointerMove(
+                        0, (double)e.x / scale, (double)e.y / scale, e.state);
+                    break;
+                }
 
-				case SDL_MOUSEBUTTONUP:
-				{
-					auto &e = event.button;
-					auto button = MouseButton(1 << (e.button - 1)); //Make one bit for each button
-					window->onPointerUp(0, button, (double)e.x / scale, (double)e.y / scale);
-				}
-				break;
-				case SDL_KEYDOWN:
-				{
-					window->onKeyDown(event.key.keysym.sym, event.key.keysym.scancode, event.key.keysym.mod, event.key.repeat);
-				}
-				break;
-				case SDL_MOUSEWHEEL:
-				{
-					auto &e = event.wheel;
-					window->onScroll(0, (double) e.x / scale, (double) e.y / scale);
-				}
-				break;
-				case SDL_KEYUP:
-				{
-					window->onKeyUp(event.key.keysym.sym, event.key.keysym.scancode, event.key.keysym.mod, event.key.repeat);
-				}
-				break;
+                case SDL_MOUSEBUTTONDOWN: {
+                    auto &e = event.button;
+                    auto button = MouseButton(
+                        1 << (e.button - 1)); // Make one bit for each button
+                    window->onPointerDown(
+                        0, button, (double)e.x / scale, (double)e.y / scale);
+                    break;
+                }
 
-				default:
-					redrawEvent = false;
-					break;
-				}
-			}
-			if (redrawEvent) {
-				window->invalidate();
-			}
-		}
-	}
-	return false;
+                case SDL_MOUSEBUTTONUP: {
+                    auto &e = event.button;
+                    auto button = MouseButton(
+                        1 << (e.button - 1)); // Make one bit for each button
+                    window->onPointerUp(
+                        0, button, (double)e.x / scale, (double)e.y / scale);
+                    break;
+                }
+
+                case SDL_MOUSEWHEEL: {
+                    auto &e = event.wheel;
+                    window->onScroll(
+                        0, (double)e.x / scale, (double)e.y / scale);
+                    break;
+                }
+
+                case SDL_KEYDOWN: {
+                    auto &key = event.key;
+                    window->onKeyDown(key.keysym.sym,
+                                      key.keysym.scancode,
+                                      key.keysym.mod,
+                                      key.repeat);
+                    break;
+                }
+
+                case SDL_KEYUP: {
+                    auto &key = event.key;
+                    window->onKeyUp(key.keysym.sym,
+                                    key.keysym.scancode,
+                                    key.keysym.mod,
+                                    key.repeat);
+                    break;
+                }
+
+                default:
+                    redrawEvent = false;
+                    break;
+                }
+            }
+            if (redrawEvent) {
+                window->invalidate();
+            }
+        }
+    }
+    return false;
 }
 
 Application::~Application() {
 #ifndef __EMSCRIPTEN__
     QuitDrawModule();
 
-    auto tmpWindowList = windows; //windows will start removing themselves from list
-    for (auto it: tmpWindowList) {
-    	delete it;
+    auto tmpWindowList =
+        windows; // windows will start removing themselves from list
+    for (auto it : tmpWindowList) {
+        delete it;
     }
     windows.clear();
 
@@ -235,62 +247,57 @@ Application::~Application() {
 #endif
 }
 
-void Application::addWindow(class Window* window) {
-	windows.push_back(window);
+void Application::addWindow(class Window *window) {
+    windows.push_back(window);
 }
 
-void Application::removeWindow(class Window* window) {
-	windows.remove(window);
-	if (windows.empty()) {
-		application->quit();
-	}
+void Application::removeWindow(class Window *window) {
+    windows.remove(window);
+    if (windows.empty()) {
+        application->quit();
+    }
 }
 
 float Application::Scale() {
-	return scale;
+    return scale;
 }
 
 void Application::Scale(float s) {
-	scale = s;
+    scale = s;
 }
 
 void Application::ContinuousUpdates(bool state) {
-	continuousUpdates = state;
+    continuousUpdates = state;
 }
 
 void Application::InvalidateOnEvent(bool state) {
-	invalidateOnEvent = state;
+    invalidateOnEvent = state;
 }
-
 
 std::string Application::GetKeyNameFromScancode(int scancode) {
-	return SDL_GetScancodeName((SDL_Scancode)scancode);
+    return SDL_GetScancodeName((SDL_Scancode)scancode);
 }
-
 
 std::string Application::GetKeyNameFromKey(int key) {
-	return SDL_GetKeyName(key);
+    return SDL_GetKeyName(key);
 }
-
 
 int Application::GetKeyFromName(const std::string &name) {
-	return SDL_GetKeyFromName(name.c_str());
+    return SDL_GetKeyFromName(name.c_str());
 }
 
-
 int Application::GetScancodeFromName(const std::string &name) {
-	return SDL_GetScancodeFromName(name.c_str());
+    return SDL_GetScancodeFromName(name.c_str());
 }
 
 int Application::GetScancodeFromKey(int key) {
-	return SDL_GetScancodeFromKey(key);
+    return SDL_GetScancodeFromKey(key);
 }
 
 int Application::GetKeyFromScancode(int scancode) {
-	return SDL_GetKeyFromScancode((SDL_Scancode)scancode);
+    return SDL_GetKeyFromScancode((SDL_Scancode)scancode);
 }
 
-
-}  // namespace MatGui
+} // namespace MatGui
 
 #endif //__ANDROID__ block
